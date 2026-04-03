66380705388656713b7aff2a391e305da6294999
Sophia Luinenburg <videogamefpslover@gmail.com>
2026-04-03 14:18:08 -0400

    More cmake fixes

---
+++ CMakeFiles/quake2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for quake2.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/ref_soft.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sophia/git/q2-mobderd/src/backends/unix/shared/hunk.c" "CMakeFiles/ref_soft.dir/src/backends/unix/shared/hunk.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/backends/unix/shared/hunk.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/models.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/models.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/models.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/pcx.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/pcx.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/pcx.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/pvs.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/pvs.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/pvs.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/stb.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/stb.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/stb.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/surf.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/surf.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/surf.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/files/wal.c" "CMakeFiles/ref_soft.dir/src/client/refresh/files/wal.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/files/wal.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_aclip.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_aclip.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_aclip.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_alias.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_alias.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_alias.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_bsp.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_bsp.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_bsp.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_draw.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_draw.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_draw.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_edge.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_edge.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_edge.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_image.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_image.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_image.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_light.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_light.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_light.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_main.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_main.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_main.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_misc.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_misc.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_misc.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_model.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_model.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_model.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_part.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_part.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_part.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_poly.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_poly.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_poly.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_polyset.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_polyset.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_polyset.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_rast.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_rast.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_rast.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_scan.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_scan.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_scan.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_sprite.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_sprite.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_sprite.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/client/refresh/soft/sw_surf.c" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_surf.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/client/refresh/soft/sw_surf.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/common/md4.c" "CMakeFiles/ref_soft.dir/src/common/md4.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/common/md4.c.o.d"
+  "/home/sophia/git/q2-mobderd/src/common/shared/shared.c" "CMakeFiles/ref_soft.dir/src/common/shared/shared.c.o" "gcc" "CMakeFiles/ref_soft.dir/src/common/shared/shared.c.o.d"
+  "" "release/ref_soft.so" "gcc" "CMakeFiles/ref_soft.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
